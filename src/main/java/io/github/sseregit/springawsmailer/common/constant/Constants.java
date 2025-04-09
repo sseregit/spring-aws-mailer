a64f03e392ca4782455d220613ed395a902122c3
장연식 <sseregit@knowwheresoft.com>
2025-04-09 11:51:03 +0900

    Constraints For Singleton

---
+++ src/main/java/io/github/sseregit/springawsmailer/common/constant/Constants.java
@@ -0,0 +1,12 @@
+package io.github.sseregit.springawsmailer.common.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class Constants {
+	public static final String COMMON_MAIL_SENDER = "sseregitjys@gmail.com";
+	public static final String OTP_ISSUER = "sseregit";
+	// QR코드를 위한 API Router
+	public static final String QR_SERVER = "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=otpauth://totp/%s:%s?secret=%s&issuer=%s";
+}
