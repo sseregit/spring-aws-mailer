99d2b5ac9a0912465965d86df7fb0fec6103b565
장연식 <sseregit@knowwheresoft.com>
2025-04-09 11:04:01 +0900

    Spring 시작하기, @SpringBootApplication

---
+++ src/main/java/io/github/sseregit/springawsmailer/SpringAwsMailerApplication.java
@@ -0,0 +1,13 @@
+package io.github.sseregit.springawsmailer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsMailerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAwsMailerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/sseregit/springawsmailer/SpringAwsMailerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sseregit.springawsmailer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsMailerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
